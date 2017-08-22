1bc5db1b32c0409fdfbee2721340f1fa7cc72ef7
Sam Parton <sam8tr@hotmail.co.uk>
2017-08-22 16:59:25 +0100

    Update

--- index.php
+++ index.php
@@ -15,6 +15,11 @@ $publicClass->sayHi(); //Will echo hello
 $publicClass->setHi('Hello Sam'); //In public classes like this, we can set variables and such
 $publicClass->sayHi();// Will echo Hello Sam
 
+//You can also do
+$publicClass->hi = "Boo!";
+$publicClass->sayHi();
+//This will echo "Boo!"
+
 //We don't initiate static classes like we do with public ones^
 //How ever, we can now call the function without "creating" it.
 StaticClass::sayHi();//Will echo hello
